82fe7ed0dc4a60e3519dfee211c04aa8871cb8c6
Busiswa Bili-bili <141039125+BUSISWABILIBILI@users.noreply.github.com>
2026-05-25 10:40:34 +0200

    feat: add ship hit method

--- tests/Ship.test.js
+++ tests/Ship.test.js
@@ -6,3 +6,11 @@ test("create a ship with a given length and 0 hits", () => {
   expect(ship.length).toBe(3);
   expect(ship.hits).toBe(0);
 });
+
+test("increments hits when hit is called", () => {
+  const ship = new Ship(3);
+
+  ship.hit();
+
+  expect(ship.hits).toBe(1);
+});
