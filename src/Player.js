6dc61c47feb76a394541008306610b8758dabcd3
Busiswa Bili-bili <141039125+BUSISWABILIBILI@users.noreply.github.com>
2026-05-25 12:03:11 +0200

    feat: add computer random attack logic

--- src/Player.js
+++ src/Player.js
@@ -1,9 +1,26 @@
 import Gameboard from "./Gameboard";
 
 class Player {
-  constructor(type) {
+  constructor(type = "real") {
     this.type = type;
     this.gameboard = new Gameboard();
+    this.previousAttacks = [];
+  }
+
+  randomAttack(enemyGameboard) {
+    let attack;
+
+    do {
+      attack = [Math.floor(Math.random() * 10), Math.floor(Math.random() * 10)];
+    } while (
+      this.previousAttacks.some(
+        (previousAttack) =>
+          previousAttack[0] === attack[0] && previousAttack[1] === attack[1],
+      )
+    );
+
+    this.previousAttacks.push(attack);
+    enemyGameboard.receiveAttack(attack);
   }
 }
 
--- tests/Player.test.js
+++ tests/Player.test.js
@@ -6,3 +6,18 @@ test("create a player with a name and an empty gameboard", () => {
   expect(player.type).toBe("real");
   expect(player.gameboard).toBeDefined();
 });
+
+test("computer can make a random attack", () => {
+  const computer = new Player("computer");
+  const enemy = new Player("real");
+
+  computer.randomAttack(enemy.gameboard);
+
+  const totalAttacks =
+    enemy.gameboard.missedAttacks.length +
+    enemy.gameboard.ships.reduce((total, shipData) => {
+      return total + shipData.ship.hits;
+    }, 0);
+
+  expect(totalAttacks).toBe(1);
+});
