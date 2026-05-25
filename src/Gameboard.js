2d7521463cd35893d697742c6c6131c5ca75f17e
Busiswa Bili-bili <141039125+BUSISWABILIBILI@users.noreply.github.com>
2026-05-25 11:00:08 +0200

    feat: add ship placement to gameboard

--- src/Gameboard.js
+++ src/Gameboard.js
@@ -1,7 +1,18 @@
+import Ship from "./Ship";
+
 class Gameboard {
   constructor() {
     this.ships = [];
     this.missedAttacks = [];
+  }
+
+  placeShip(length, coordinates) {
+    const ship = new Ship(length);
+
+    this.ships.push({
+      ship,
+      coordinates,
+    });
   }
 }
 
--- tests/Gameboard.test.js
+++ tests/Gameboard.test.js
@@ -6,3 +6,20 @@ test("create an empty gameboard", () => {
   expect(gameboard.ships).toEqual([]);
   expect(gameboard.missedAttacks).toEqual([]);
 });
+
+test("places a ship at given coordinates", () => {
+  const gameboard = new Gameboard();
+
+  gameboard.placeShip(3, [
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ]);
+
+  expect(gameboard.ships.length).toBe(1);
+  expect(gameboard.ships[0].coordinates).toEqual([
+    [0, 0],
+    [0, 1],
+    [0, 2],
+  ]);
+});
